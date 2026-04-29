7ced774a980cf27923496c8b794808794bf61f4b
Poojitha <poojithadinesh293@gmail.com>
2026-04-29 06:26:18 +0530

    debug mongo

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  const nongoUri = process.env.MONGO_URL ||"mongo://127.0.0.1:27017/vemu-sams";
+  const nongoUri = process.env.MONGO_URL;
+  console.log("connecting to:", mongoUri); //||"mongo://127.0.0.1:27017/vemu-sams";//
   /*const mongoUri = process.env.MONGO_URI;
   mongoose.set("strictQuery", true);*/
   mongoose.set("strictQuery",true);
