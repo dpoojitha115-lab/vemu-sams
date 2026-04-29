28114d066eb9f558c5124e85f26be619ad8ec3b4
Poojitha <poojithadinesh293@gmail.com>
2026-04-29 05:50:27 +0530

    fix db uri

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  
-  const mongoUri = process.env.MONGO_URI;
-  mongoose.set("strictQuery", true);
+  const nongoUri = process.env.MONGO_URL ||"mongo://127.0.0.1:27017/vemu-sams";
+  /*const mongoUri = process.env.MONGO_URI;
+  mongoose.set("strictQuery", true);*/
+  mongoose.set("strictQuery",true);
   await mongoose.connect(mongoUri);
   return mongoose.connection;
 }
