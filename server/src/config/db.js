93071b1239f78be9c7e540e9f5ab14d694a20e3e
Poojitha <poojithadinesh293@gmail.com>
2026-04-29 06:40:03 +0530

    debug mongo

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,26 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 
 async function connectDB() {
   const nongoUri = process.env.MONGO_URL;
-  console.log("connecting to:", mongoUri); //||"mongo://127.0.0.1:27017/vemu-sams";//
+  console.log("connecting to:", mongoUri); *///||"mongo://127.0.0.1:27017/vemu-sams";//
   /*const mongoUri = process.env.MONGO_URI;
   mongoose.set("strictQuery", true);*/
-  mongoose.set("strictQuery",true);
+  /*mongoose.set("strictQuery",true);
   await mongoose.connect(mongoUri);
   return mongoose.connection;
 }
 
-module.exports = { connectDB };
+module.exports = { connectDB };*/
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
